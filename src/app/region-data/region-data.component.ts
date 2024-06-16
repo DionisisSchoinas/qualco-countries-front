b4262eeb525796ebbb0dbada06aa000bae59ae2a
Schoinas Dionisis <schinasdionisis@gmail.com>
2024-06-16 22:48:14 +0300

    Pagination completed

--- src/app/region-data/region-data.component.ts
+++ src/app/region-data/region-data.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Region } from '../models/region.model';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegionStats } from '../models/region-stats.model';
+import { NgbHighlight, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { Paged } from '../models/paged.model';
 
 @Component({
   selector: 'app-region-data',
   standalone: true,
-  imports: [HttpClientModule, ReactiveFormsModule],
+  imports: [HttpClientModule, ReactiveFormsModule, NgbHighlight, NgbPaginationModule, FormsModule],
   templateUrl: './region-data.component.html',
   styleUrl: './region-data.component.css'
 })
 export class RegionDataComponent {
   
   public regions: Region[] = [];
-  public regionStats: RegionStats[] = [];
+  
+  public paged?: Paged;
+
+  public page: number = 1;
+  public pageSize: number= 10;
 
   httpClient: HttpClient;
   form?: any;
@@ -39,12 +45,18 @@ export class RegionDataComponent {
       );
   }  
 
-  public onSubmit() {
-    console.log(this.form.value);
+  load() {
     this.httpClient
-      .post("http://localhost:9095/regions", this.form.value)
+      .post("http://localhost:9095/regions", {
+          "regionId": this.form.value.regionId,
+          "dateFrom": this.form.value.dateFrom,
+          "dateTo": this.form.value.dateTo,
+          "page": this.page-1, 
+          "size": this.pageSize
+        }
+      )
       .subscribe({
-          next: (data: any) => this.regionStats = data,
+          next: (data: any) => this.paged = data,
           error: (err) => console.log(err)
         }
       );
