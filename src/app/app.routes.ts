fdd6a84943c187f71f3f284bb66befa4f487411f
Schoinas Dionisis <schinasdionisis@gmail.com>
2024-06-16 15:49:03 +0300

    Home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { RegionDataComponent } from './region-data/region-data.component';
 import { CountryGdpComponent } from './country-gdp/country-gdp.component';
 import { CountryComponent } from './country/country.component';
 import { CountryLanguagesComponent } from './country-languages/country-languages.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
+    {path: '', component: HomePageComponent},
     {path: 'region-data', component: RegionDataComponent},
     {path: 'country-data', component: CountryComponent},
     {path: 'country-data/:countryId/languages', component: CountryLanguagesComponent},
