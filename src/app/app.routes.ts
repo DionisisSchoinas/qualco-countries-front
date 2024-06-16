6b2c274bab731947daa64cdefabd03d71049de03
Schoinas Dionisis <schinasdionisis@gmail.com>
2024-06-16 12:21:46 +0300

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegionDataComponent } from './region-data/region-data.component';
+import { CountryLanguagesComponent } from './country-languages/country-languages.component';
+import { CountryGdpComponent } from './country-gdp/country-gdp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'region-data', component: RegionDataComponent},
+    {path: 'country-languages', component: CountryLanguagesComponent},
+    {path: 'country-gdp', component: CountryGdpComponent},
+];
